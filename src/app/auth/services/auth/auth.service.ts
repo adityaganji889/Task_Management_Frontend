732929888209e0a7c9ae36db58ccde6c51351c72
adityaganji889 <adityaganji889@gmail.com>
2024-05-30 03:12:50 +0530

    Updated

--- src/app/auth/services/auth/auth.service.ts
+++ src/app/auth/services/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = "https://taskmanagementbackend-production-4883.up.railway.app"
 
 @Injectable({
   providedIn: 'root'
